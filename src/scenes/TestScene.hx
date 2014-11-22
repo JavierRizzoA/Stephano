2d0c8582eb42796650f9c68e17e35e57a0291c44
Javier Rizzo <javierrizzoa@gmail.com>
2014-11-22 14:36:44 -0800

    Modify TestScene to test physics

--- src/scenes/TestScene.hx
+++ src/scenes/TestScene.hx
@@ -8,13 +8,17 @@ class TestScene extends Scene
 {
 	public override function begin()
 	{
-		var player:entities.Player = new entities.Player(0, 0);
+		var player:entities.Player = new entities.Player(350, 0);
 
 		add(player);
 
 		for(i in 0 ... 10) {
 			addGraphic(new Image("graphics/block.png"), 0, i*70, HXP.height - 70);
 			add(new entities.Solid(i*70, HXP.height - 70));
+			addGraphic(new Image("graphics/block.png"), 0, HXP.width - 70, HXP.height - 70*(i+1));
+			add(new entities.Solid(HXP.width - 70, HXP.height - 70*(i+1)));
+			addGraphic(new Image("graphics/block.png"), 0, 0, HXP.height - 70*(i+1));
+			add(new entities.Solid(0, HXP.height - 70*(i+1)));
 		}
 	}
 }
