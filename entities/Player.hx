6d05b9ce282fec5e454a6b2fbd2d2d7e20518ea4
raultcj <raultcj@outlook.com>
2014-11-21 21:58:47 -0800

    Added image files.
    Renamed

--- entities/Player.hx
+++ entities/Player.hx
@@ -3,5 +3,11 @@ package entities;
 import com.haxepunk.Entity;
 
 class Player extends Entity {
-	
+
+	public function new(x:Float, y:Float)
+	{
+		super(x, y);
+
+		graphic = 
+	}
 }
--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,9 +1,10 @@
 import com.haxepunk.Scene;
+import entities.Player;
 
 class MainScene extends Scene
 {
 	public override function begin()
 	{
-
+		add(new Player(x:700, y:HXP.scene.halfWidth))
 	}
 }
